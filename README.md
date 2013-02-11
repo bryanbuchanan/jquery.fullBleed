43731c860621d564fd496a2376b788b428a4d6f1
Bryan Buchanan <bryanbuchanan@Bryans-MacBook-Pro.local>
2013-02-10 21:37:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# jquery.fullBleed
+
+Scales an image to take up the entire browser window.
+
+## Usage
+
+```javascript
+$('img').fullBleed();
+```
+
+The alignment of the image can be changed with the `align` property:
+
+```javascript
+$('img').fullBleed({ align: 'top left' );
+```
+
+Possible values for `align` are:
+
+- top left
+- top center
+- top right
+- center left
+- center center (this is the default)
+- center right
+- bottom left
+- bottom center
+- bottom right
---
+++ jquery.fullbleed.js
@@ -0,0 +1,108 @@
+(function($) {
+
+	$.fn.fullBleed = function(parameters) {
+
+		// Defaults
+		parameters = parameters || {};
+		var defaults = { align: 'center center' };
+		parameters = $.extend(defaults, parameters);
+
+		// Preserve jQuery chaining
+		return this.each(function() {
+
+			var $image = $(this);		
+			var $window = $(window);
+
+			// Style precedents
+			$image.parent().css({ overflow: 'hidden' });
+			$image.css({ position: 'absolute' });
+	
+			function resize() {
+
+				// Get numbers to work with
+				var width = $image.data('width');
+				var height = $image.data('height');
+				var windowWidth = parseInt($window.width());
+				var windowHeight = parseInt($window.height());
+				var aspectRatio = Math.round(width / height * 100) / 100;
+
+				// Define new size based on width
+				var newWidth = windowWidth;
+				var newHeight = Math.round(newWidth / aspectRatio);
+	
+				// Or, define new size based on height
+				if (newHeight < windowHeight) {
+					var newHeight = windowHeight;
+					var newWidth = newHeight * aspectRatio
+				}
+	
+				// Positioning options
+				switch (parameters.align) {
+					case "top left":
+						var newPosition = [0, 'auto', 'auto', 0];
+						break;
+					case "top center":
+						var newPosition = [0, 'auto', 'auto', (windowWidth - newWidth) / 2 + 'px'];
+						break;
+					case "top right":
+						var newPosition = [0, 0, 'auto', 'auto'];
+						break;
+					case "center left":
+						var newPosition = [(windowHeight - newHeight) / 2 + "px", 'auto', 'auto', 0];
+						break;
+					case "center center":
+						var newPosition = [(windowHeight - newHeight) / 2 + 'px', 'auto', 'auto', (windowWidth - newWidth) / 2 + 'px'];
+						break;
+					case "center right":
+						var newPosition = [(windowHeight - newHeight) / 2 + "px", 0, 'auto', 'auto'];
+						break;
+					case "bottom left":
+						var newPosition = ['auto', 'auto', 0, 0];
+						break;
+					case "bottom center":
+						var newPosition = ['auto', 'auto', 0, (windowWidth - newWidth) / 2 + 'px'];
+						break;
+					case "bottom right":
+						var newPosition = ['auto', 0, 0, 'auto'];
+						break;
+				}	
+				
+				// Resize and position
+				$image.css({
+					'width': newWidth + 'px',
+					'height': newHeight + 'px',
+					'top': newPosition[0],
+					'right': newPosition[1],
+					'bottom': newPosition[2],
+					'left': newPosition[3]
+				});
+	
+			}
+	
+			// Fire when window is resized
+			$(window).resize(function() { resize($image); });
+
+			// Fire when image is loaded
+			$image.load(function() {
+				$this = $(this);
+				// Save original image dimensions
+				$this.data({
+					'width': parseInt($this.width()),
+					'height': parseInt($this.height())
+				});
+				// Initial resize
+				resize($this);
+				// Show image
+				$this.css({ opacity: 1 });
+			}).each(function() {
+				// Fire load function anyway if image is grabbed from the cache
+				if (this.complete) $(this).load();
+			});
+	
+			return this;
+
+		});
+
+	};
+
+})(jQuery);
